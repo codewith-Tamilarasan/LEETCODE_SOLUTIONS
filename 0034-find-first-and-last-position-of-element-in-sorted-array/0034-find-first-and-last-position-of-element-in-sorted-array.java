748891ce68dcb6237ffe56ff5483e597e8842a4a
codewith-Tamilarasan <135610699+codewith-Tamilarasan@users.noreply.github.com>
2023-09-20 06:38:15 +0530

    Time: 4 ms (20.41%), Space: 44.2 MB (87.77%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,31 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int c = 0;
-        int arr[] = new int[2];
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==target)
-            {
-                if(c==0)
-                arr[0] = i;
-                else
-                arr[1] = i;
-                c++;
-            }
-        }
+//         int arr[] = new int[2];
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i]==target)
+//             {
+//                 if(c==0)
+//                 arr[0] = i;
+//                 else
+//                 arr[1] = i;
+//                 c++;
+//             }
+//         }
         
-        if(c==0)
-        {
-           arr[0] = -1;
-           arr[1] = -1;
-        }
+//         if(c==0)
+//         {
+//            arr[0] = -1;
+//            arr[1] = -1;
+//         }
 
-        if(c==1)
-        arr[1] = arr[0];
+//         if(c==1)
+//         arr[1] = arr[0];
         
        
 
         
-        return arr;
+//         return arr;
+        
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for(int x:nums)
+                a.add(x);
+        int x = a.lastIndexOf(target);
+        int y = a.indexOf(target);
+        
+        return new int[] {y,x};
     }
 }
